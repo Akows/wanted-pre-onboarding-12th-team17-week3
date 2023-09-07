489178b7067fa1c7301d56b1e08cd9399d618af0
AKOWSPC <akows141@gmail.com>
2023-09-07 16:45:58 +0900

    Design: 헤더 컴포넌트 UI 구현 및 추가.

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import { SearchBar } from '../components/SearchBar';
+import logoImage from '../assets/logo.svg';
 
 const MainPage: React.FC = () => {
   return (
     <MainPageWrapper>
+      <Header>
+        <Logo src={logoImage} alt="Company Logo" />
+      </Header>
       <MainTitle>국내 모든 임상시험 검색하고</MainTitle>
       <MainTitle>온라인으로 참여하기</MainTitle>
       <SearchBar />
@@ -28,6 +32,22 @@ const MainPageWrapper = styled.div`
     margin-top: 120px;
     margin-bottom: 10px;
   }
+`;
+
+const Header = styled.header`
+  width: 100%;
+  height: 60px; // 원하는 높이로 설정하세요
+  background-color: white;
+  display: flex;
+  align-items: center;
+  top: 0;
+  margin-bottom: 50px;
+`;
+
+const Logo = styled.img`
+  width: 150px;
+  height: 50px;
+  margin-left: 20px; // 로고의 좌측 여백 설정
 `;
 
 const MainTitle = styled.p`
